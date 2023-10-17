f9bb974bf1a80401b36ea3ad39618c0be5345452
ᴢᴇɴσᴠ <143601779+RealZenX@users.noreply.github.com>
2023-10-17 22:13:50 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -43,7 +43,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [RIO NETWORKS™](https://t.me/creatorrio)**"
+        cap = "**BY›› [TeamSugoiX™](https://t.me/teamsugoix)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
