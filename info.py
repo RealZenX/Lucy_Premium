869ba0df2652abd0d3fe8a9c562bda8d958b8428
ᴢᴇɴσᴠ <143601779+RealZenX@users.noreply.github.com>
2023-10-31 21:02:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mikey:mikey@cluster0.z79qxeq.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Wednesdayx:Wednesdayx@cluster0.tgx9odc.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Wednesdayx")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
